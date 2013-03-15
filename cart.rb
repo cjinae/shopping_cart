337cb6c3dd2dbffeb001e83d70cbe8afc084f28b
Bitmaker Labs <bitmakerlabs@Bitmakers-iMac-2.local>
2013-03-15 17:56:27 -0400

    fixed bug: added until loop, created class property in check_out, removed item from initialized and added it to validation check, made item a instance variable.

--- cart.rb
+++ cart.rb
@@ -4,28 +4,42 @@ class Cart
 	# It is not responsible for human interaction
 	class InvalidGuessException < StandardError 
 	end
+	class Checkout < StandardError
+	end
 
-	attr_accessor :all_product_names, :all_product_prices, :library, :product_name, :prices, :customer_items 	# attr_accessor will create for me both a method and a parameter
+	attr_accessor :all_product_names, :all_product_prices, :library, :product_name, :prices, :customer_items, :item 	# attr_accessor will create for me both a method and a parameter
 
-	def initialize (item)					#  you need to pass the information data to the Cart
+	def initialize					#  you need to pass the information data to the Cart
 		puts "Bonjour!"
 		@all_product_names = []
 		@library = []
-		# @product_name = ""
+		@item = ""
 		@price = ""
 		@all_prices = []
 		@all_product_names = ["apple", "orange", "milk"]
 		@customer_items = []
 	end
 
-	def add_items_to_cart(item)				
-		@customer_items << item			
+	def check_for_valid_item(item)
+		@item = item
+		# puts "checking item array" + @all_product_names.include?(@item).to_s			# debugging tool by using puts ""
+		if @item == "cash me out"
+			cash_out?
+		elsif	@all_product_names.include?(@item) == false
+				raise InvalidGuessException.new("Sorry! #{@item} is not for sale")
+			# elsif item == "I have no money!"
+			# 	raise InvalidGuessException.new("That's too bad. See you next time.")
+		end
 	end
 
-	def find_price(item)
-		puts item
+	def add_items_to_cart			
+		@customer_items << @item			
+	end
+
+	def find_price
+		puts @item
 		@library = [[2,"apple"], [1.5,"orange"], [4,"milk"], [3.5,"eggs"], [5,"bread"]]
-		@price = (@library.rassoc(item))[0]
+		@price = (@library.rassoc(@item))[0]
 	end
 
 	def collect_prices
@@ -43,18 +57,11 @@ class Cart
 	# 	end
 	# end
 
-	def check_for_valid_item(item)
-		puts "checking item array" + @all_product_names.include?(item).to_s			# debugging tool by using puts ""
-		if @all_product_names.include?(item) == false
-				raise InvalidGuessException.new("Sorry! #{item} is not for sale")
-			# elsif item == "I have no money!"
-			# 	raise InvalidGuessException.new("That's too bad. See you next time.")
-		end
-	end
 
-	def cash_out(item)
-		if item == "cash me out"
+	def cash_out?
+		if @item == "cash me out"
 			true
+			raise Checkout.new("Goodbye")
 		end
 	end
 
--- check_out.rb
+++ check_out.rb
@@ -9,12 +9,14 @@ class Checkout
 
 # start checkout
 	def self.start
-		@@checkout = Cart.new("")
-		puts "Give a product to the cashier"
-		product_name = gets.chomp.downcase
-		while !@@checkout.cash_out(product_name)	# change this later							
+		@@checkout = Cart.new
+		until @@checkout.cash_out?	# change this later	
+			puts "Give a product to the cashier"
+			@@product_name = gets.chomp.downcase						
 			self.show_results						# if i did a self.show_results(prodduct_name) I can passes the parameter product_name to show_results, don't need to use @@ all the time
-		end			
+		end
+		rescue Cart::Checkout => e											# exception rule only occurs within begin
+	  		puts e.message
 	end
 
 # show results
@@ -24,11 +26,12 @@ class Checkout
 			# puts "Give a product to the cashier"
 			# product_name = gets.chomp.downcase
 			# while !@@checkout.cash_out?	 	
-				puts "Item just scanned is #{product_name}"
-				@@checkout.check_for_valid_item(product_name)  							# raises the exception here 
-				@@checkout.add_items_to_cart(product_name)
+				@@checkout.check_for_valid_item(@@product_name)							# raises the exception here 
+				@@checkout.add_items_to_cart
+
+				puts "Item just scanned is #{@@product_name}"
 			
-				puts "Price of #{product_name} is $#{@@checkout.find_price(product_name)}"
+				puts "Price of #{@@product_name} is $#{@@checkout.find_price}"
 				puts "These are all the prices in an array #{@@checkout.collect_prices}"   		#remove later
 				puts "This is your current bill #{@@checkout.add_all_prices}"
 				puts "Here is your current grocery list #{(@@checkout.customer_items)}"
